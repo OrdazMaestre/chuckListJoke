3d1fcf2770f12fee22da1c4e3152e1af0030b7cd
OrdazMaestre <ordazmaestre@gmail.com>
2025-11-21 20:44:58 +0100

    Co-authored-by: MarcGP98 <MarcGP98@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-                  
+                 
 const button = document.getElementById("fetchJoke");    //seleccionamos el boton
 const list = document.getElementById("jokeList");       //seleccionamos la <ul>
 
@@ -11,26 +11,3 @@ button.addEventListener("click", async () => {
 
     list.appendChild(li);
 });
-
-
-
-/* VERSION QUE MANDA ERROR SI ALGO NO FUNCIONA CON UN "TRY{...}CATCH(err){...}"
-
-const button = document.getElementById("fetchJoke");    //seleccionamos el boton
-const list = document.getElementById("jokeList");       //seleccionamos la <ul>
-
-button.addEventListener("click", async () => {
-  try {
-    const res = await fetch("https://api.chucknorris.io/jokes/random");
-    const data = await res.json();
-    const li = document.createElement("li");
-    
-    li.textContent = data.value;
-
-    list.appendChild(li);
-  } catch (err) {
-    console.error("Error al obtener el chiste:", err);
-  }
-});                         
-
-*/          
