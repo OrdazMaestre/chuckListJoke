37dc26ef7b49a84035c77db7083ca9271edf5555
OrdazMaestre <ordazmaestre@gmail.com>
2025-11-21 19:22:49 +0100

    ChukListFinish

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-const button = document.getElementById("fetchJoke");
-const list = document.getElementById("jokeList");
+                  
+const button = document.getElementById("fetchJoke");    //seleccionamos el boton
+const list = document.getElementById("jokeList");       //seleccionamos la <ul>
+
+button.addEventListener("click", async () => {
+    const res = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await res.json();
+    const li = document.createElement("li");
+    
+    li.textContent = data.value;
+
+    list.appendChild(li);
+});
+
+
+
+/* VERSION QUE MANDA ERROR SI ALGO NO FUNCIONA CON UN "TRY{...}CATCH(err){...}"
+
+const button = document.getElementById("fetchJoke");    //seleccionamos el boton
+const list = document.getElementById("jokeList");       //seleccionamos la <ul>
 
 button.addEventListener("click", async () => {
   try {
     const res = await fetch("https://api.chucknorris.io/jokes/random");
     const data = await res.json();
-
     const li = document.createElement("li");
+    
     li.textContent = data.value;
 
     list.appendChild(li);
   } catch (err) {
     console.error("Error al obtener el chiste:", err);
   }
-});
+});                         
+
+*/          
